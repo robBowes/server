0d40c1db36d87704ace7d257a6c9659ecef6cd65
Robert Bowes <robert@vesselfunds.com>
2024-01-18 20:30:26 -0500

    create monorepo

--- apps/dashboard/svelte.config.js
+++ apps/dashboard/svelte.config.js
@@ -2,6 +2,7 @@ import { preprocessMeltUI } from '@melt-ui/pp';
 import sequence from 'svelte-sequential-preprocessor';
 import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+
 /** @type {import('@sveltejs/kit').Config}*/
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
