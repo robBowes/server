ed1446dad6ad036d6d9e8f186dd462ddd4816acc
Robert Bowes <robert@vesselfunds.com>
2024-01-18 21:13:51 -0500

    Henri

---
+++ apps/dashboard/src/routes/+layout.svelte
@@ -0,0 +1,5 @@
+<slot />
+
+<style>
+	@import '../../node_modules/reset-css/reset.css';
+</style>
---
+++ apps/dashboard/src/routes/henri/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	let backgroundColour = '#fff';
+
+	function handleClick() {
+		backgroundColour = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+	}
+</script>
+
+<div class="canvas" style:background-color={backgroundColour} on:click={handleClick} />
+
+<style>
+	.canvas {
+		width: 100%;
+		height: 100vh;
+	}
+</style>
