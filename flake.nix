9ed8ba2de1c7097a4dae79588f893da346c4d989
Robert Bowes <r.e.bowes@gmail.com>
2025-10-08 01:31:08 +0000

    Change to nixos

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
 
   outputs = { self, nixpkgs }: {
     nixosConfigurations = {
-      home = nixpkgs.lib.nixosSystem {
+      nixos = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
           ./configuration.nix
